483b8ef1ba2b23d214856c8ec640e3f6524dcbb2
ainhoacr <ainhoa.c.rodriguez@gmail.com>
2015-09-18 16:18:52 +0200

    zoom layout funcionando y creada la clase del coverFlow

---
+++ Week2/Dia 5/Proyecto GOT/GOTCollectionView/CoverFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.m
+//  GOTCollectionView
+//
+//  Created by Ainhoa Calviño Rodriguez on 18/09/15.
+//  Copyright (c) 2015 Ricardo Sanchez. All rights reserved.
+//
+
+#import "CoverFlowLayout.h"
+
+@implementation CoverFlowLayout
+
+@end
