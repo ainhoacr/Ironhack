dfb451a657494c72153ecba46e081360a006c2a3
ainhoacr <ainhoa.c.rodriguez@gmail.com>
2015-09-22 14:17:11 +0200

    ejercicios de por la mañana

---
+++ Week3/Dia 2/AutoSizing/AutoSizing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoSizing
+//
+//  Created by Ainhoa Calviño Rodriguez on 22/09/15.
+//  Copyright © 2015 Ainhoa Calviño Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week3/Dia 2/AutoSizing/AutoSizing/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoSizing
+//
+//  Created by Ainhoa Calviño Rodriguez on 22/09/15.
+//  Copyright © 2015 Ainhoa Calviño Rodriguez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
